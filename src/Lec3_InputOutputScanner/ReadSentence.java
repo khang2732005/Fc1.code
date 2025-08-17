67d9b407c400bf9adc21e0e0736966dbc3f2d368
Khang <Khang@DESKTOP-F52S1DE>
2025-08-17 15:33:31 +0700

    test

---
+++ src/Lec3_InputOutputScanner/ReadSentence.java
@@ -0,0 +1,15 @@
+package Lec3_InputOutputScanner;
+
+import java.util.Scanner;
+
+public class ReadSentence {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+
+        System.out.println("You entered: " + sentence);
+    }
+}
